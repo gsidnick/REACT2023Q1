74976b523ed29ebc0c6fbdab85fc76cfdbf16352
Nick GS <gsidnick@gmail.com>
2023-03-14 15:00:30 +0300

    feat: add content section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Content from './components/Content/Content';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Content />
         <Footer />
       </div>
     );
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,14 @@
+import './Content.css';
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main className="content">
+        <div className="content__container container">CONTENT</div>
+      </main>
+    );
+  }
+}
+
+export default Content;
