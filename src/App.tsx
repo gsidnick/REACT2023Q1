bd0d4d03b948fa7b0848a88e232d38c7f434fef3
Nick GS <gsidnick@gmail.com>
2023-03-14 14:56:41 +0300

    feat: add footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Footer />
       </div>
     );
   }
