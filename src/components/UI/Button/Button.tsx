c6dacf0b0e2b19f9280d957e2afef927b1a54fd2
Nick GS <gsidnick@gmail.com>
2023-03-14 15:55:55 +0300

    feat: add Button component

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,10 @@
+import './Button.css';
+import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return <button className="button">Add to cart</button>;
+  }
+}
+
+export default Button;
