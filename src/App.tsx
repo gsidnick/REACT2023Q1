8334dad7d9cd4ef314be3c8755dad3ee0af76804
Nick GS <gsidnick@gmail.com>
2023-03-15 10:31:40 +0300

    feat: add router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Catalog from './components/Catalog/Catalog';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 import Content from './components/Content/Content';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Content />
-        <Footer />
+        <Router>
+          <Header />
+          <Content>
+            <Routes>
+              <Route path="/" element={<Catalog />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Content>
+          <Footer />
+        </Router>
       </div>
     );
   }
