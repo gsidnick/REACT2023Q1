1e6e884336c4bf8ba7692a3fc4d0de6e87134169
Nick GS <gsidnick@gmail.com>
2023-03-16 12:57:15 +0300

    feat: add query save to localStorage

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -3,12 +3,33 @@ import React from 'react';
 import Input from '../UI/Input/Input';
 import List from '../List/List';
 
-class Catalog extends React.Component {
+class Catalog extends React.Component<object, { query: string }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { query: '' };
+    this.searchChangeHandler = this.searchChangeHandler.bind(this);
+  }
+  componentDidMount() {
+    const query = localStorage.getItem('query');
+    if (query !== null) this.setState({ query });
+  }
+
+  componentWillUnmount() {
+    if (this.state.query !== '') {
+      localStorage.setItem('query', this.state.query);
+    } else {
+      localStorage.removeItem('query');
+    }
+  }
+
+  searchChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ query: event.target.value });
+  }
   render() {
     return (
       <div className="catalog">
         <div className="catalog__search">
-          <Input />
+          <Input value={this.state.query} onChange={this.searchChangeHandler} />
         </div>
         <div className="catalog__products">
           <List />
