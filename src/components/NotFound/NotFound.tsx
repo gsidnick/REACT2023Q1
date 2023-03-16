2f980e8bc74c7295e82570484de25e52939cf5ec
Nick GS <gsidnick@gmail.com>
2023-03-16 11:03:11 +0300

    feat: add navigate to button

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,9 +1,13 @@
 import './NotFound.css';
 import React from 'react';
 import Button from '../UI/Button/Button';
+import { IWithRouterProps } from '../../hoc/IWithRouterProps';
+import withRouter from '../../hoc/withRouter';
 
-class NotFound extends React.Component {
+class NotFound extends React.Component<IWithRouterProps> {
   render() {
+    const navigate = this.props.navigate;
+
     return (
       <div className="not-found">
         <h1 className="not-found__title">404</h1>
@@ -12,10 +16,12 @@ class NotFound extends React.Component {
           <p>We didn’t find this page.</p>
           <p>But we know where to find a lot of delicious things.</p>
         </span>
-        <Button className="not-found__button">Go to home</Button>
+        <Button className="not-found__button" onClick={() => navigate('/')}>
+          Go to home
+        </Button>
       </div>
     );
   }
 }
 
-export default NotFound;
+export default withRouter(NotFound);
