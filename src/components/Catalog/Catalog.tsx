2799ec00cf3d28942b8841c3846625f0296e41b5
Nick GS <gsidnick@gmail.com>
2023-03-17 17:59:43 +0300

    feat: add products filter

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -2,28 +2,49 @@ import './Catalog.css';
 import React from 'react';
 import Input from '../UI/Input/Input';
 import List from '../List/List';
+import data from '../../../data.json';
+import IProduct from '../../interfaces/IProduct';
+import { getQuery, setQuery } from '../../utils/localStorage';
 
-class Catalog extends React.Component<object, { query: string }> {
+class Catalog extends React.Component<object, { query: string; products: IProduct[] }> {
+  private products: IProduct[];
   constructor(props: object) {
     super(props);
-    this.state = { query: '' };
+    this.products = data;
+    this.state = { query: '', products: [] as IProduct[] };
     this.searchChangeHandler = this.searchChangeHandler.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
   }
   componentDidMount() {
-    const query = localStorage.getItem('query');
-    if (query !== null) this.setState({ query });
+    let query = getQuery();
+    if (query === null) query = '';
+    this.setState({ query });
+    this.applyFilter(query);
   }
 
   componentWillUnmount() {
-    if (this.state.query !== '') {
-      localStorage.setItem('query', this.state.query);
-    } else {
-      localStorage.removeItem('query');
-    }
+    setQuery(this.state.query);
   }
 
   searchChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     this.setState({ query: event.target.value });
+    this.applyFilter(event.target.value);
+    setQuery(event.target.value);
+  }
+
+  applyFilter(query: string) {
+    if (query === '') {
+      this.setState({ products: this.products });
+      return;
+    }
+    const filteredProducts = this.products.filter((product) => {
+      const name = product.name.toLowerCase();
+      const description = product.description.toLowerCase();
+      const price = product.price;
+      const q = query.toLowerCase();
+      return name.includes(q) || price.toString().includes(q) || description.includes(q);
+    });
+    this.setState({ products: filteredProducts });
   }
   render() {
     return (
@@ -32,7 +53,7 @@ class Catalog extends React.Component<object, { query: string }> {
           <Input value={this.state.query} onChange={this.searchChangeHandler} />
         </div>
         <div className="catalog__products">
-          <List />
+          <List value={this.state.products} />
         </div>
       </div>
     );
