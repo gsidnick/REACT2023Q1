fb76ea4f187e9cb33298210d3d133f336a006764
Nick GS <gsidnick@gmail.com>
2023-03-17 16:23:03 +0300

    feat: add value props to List component

---
+++ src/components/List/IListProps.ts
@@ -0,0 +1,5 @@
+import IProduct from '../../interfaces/IProduct';
+
+export interface IListProps {
+  value: IProduct[];
+}
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,15 +1,18 @@
 import './List.css';
 import React from 'react';
 import Card from '../Card/Card';
+import { IListProps } from './IListProps';
 
-class List extends React.Component {
+class List extends React.Component<IListProps> {
+  constructor(props: IListProps) {
+    super(props);
+  }
   render() {
     return (
       <div className="list">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {this.props.value.map((product) => (
+          <Card key={product.id} value={product} />
+        ))}
       </div>
     );
   }
