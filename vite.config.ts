c8f3fda3f2625da7ea0323f9b928ce8bda35ebc0
Nick GS <gsidnick@gmail.com>
2023-03-19 11:09:15 +0300

    feat: install testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      enabled: true,
+      provider: 'c8',
+      reporter: ['text'],
+      all: true,
+      exclude: [
+        ...configDefaults.exclude,
+        '**/src/tests/*',
+        '**/src/interfaces/*',
+        '**/src/utils/*',
+        '**/src/hoc/*',
+      ],
+    },
+  },
 });
