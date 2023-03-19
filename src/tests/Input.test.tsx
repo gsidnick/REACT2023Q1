78c92ab6c16380dd1186f029d0c52e1cf3c5c51c
Nick GS <gsidnick@gmail.com>
2023-03-19 13:10:20 +0300

    feat: add Input test

---
+++ src/tests/Input.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Input from '../components/UI/Input/Input';
+
+describe('Testing <Input /> component', () => {
+  test('should show Input component with input text', () => {
+    render(<Input value="Type text here..." onChange={() => undefined} />);
+    expect(screen.getByText(/.*/i, { selector: '.input__field' })).toBeDefined();
+  });
+});
