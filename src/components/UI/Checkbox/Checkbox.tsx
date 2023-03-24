53591664517f8b9c5cf3d0b43358f84c236dce19
Nick GS <gsidnick@gmail.com>
2023-03-24 12:43:49 +0300

    feat: add Checkbox component

---
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -0,0 +1,32 @@
+import './Checkbox.css';
+import React from 'react';
+import { ICheckboxProps } from '../../../interfaces/ICheckboxProps';
+import IconCheck from '../Icon/IconCheck/IconCheck';
+
+class Checkbox extends React.Component<ICheckboxProps> {
+  constructor(props: ICheckboxProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <label className="checkbox">
+        <input
+          className="checkbox__field"
+          type="checkbox"
+          name={this.props.name}
+          ref={this.props.forwardedRef}
+        />
+        <span className="checkbox__wrapper">
+          <span className="checkbox__icon">
+            <IconCheck />
+          </span>
+          <span className="checkbox__label">{this.props?.children}</span>
+        </span>
+      </label>
+    );
+  }
+}
+
+export default React.forwardRef<HTMLInputElement, ICheckboxProps>((props, ref) => (
+  <Checkbox forwardedRef={ref} {...props} />
+));
