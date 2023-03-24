0b411fccd5e179e2ed2822e65096450ba606dc4b
Nick GS <gsidnick@gmail.com>
2023-03-24 13:44:10 +0300

    feat: add Radio & Checkbox component to Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import InputFile from '../UI/InputFile/InputFile';
 import InputText from '../UI/InputText/InputText';
 import Dropdown from '../UI/Dropdown/Dropdown';
+import Checkbox from '../UI/Checkbox/Checkbox';
+import Radio from '../UI/Radio/Radio';
 
 class Form extends React.Component {
   private readonly formRef: React.RefObject<HTMLFormElement>;
@@ -10,6 +12,7 @@ class Form extends React.Component {
   private readonly nameRef: React.RefObject<HTMLInputElement>;
   private readonly emailRef: React.RefObject<HTMLInputElement>;
   private readonly imageRef: React.RefObject<HTMLInputElement>;
+  private readonly agreeRef: React.RefObject<HTMLInputElement>;
 
   constructor(props: object) {
     super(props);
@@ -18,6 +21,7 @@ class Form extends React.Component {
     this.nameRef = React.createRef();
     this.emailRef = React.createRef();
     this.imageRef = React.createRef();
+    this.agreeRef = React.createRef();
   }
 
   render() {
@@ -27,6 +31,13 @@ class Form extends React.Component {
         <InputText ref={this.nameRef} name="name" placeholder="Name" />
         <InputText ref={this.emailRef} name="email" placeholder="E-mail" />
         <InputFile ref={this.imageRef} name="image" label="Choose photo" />
+        <div className="form__question">
+          <span className="form__question-label">What are you ready to check?</span>
+          <Radio name="radio">Restaurant and Delivery</Radio>
+          <Radio name="radio">Cuisine</Radio>
+          <Radio name="radio">All together</Radio>
+        </div>
+        <Checkbox name="check1">I agree to the processing of personal data</Checkbox>
       </form>
     );
   }
