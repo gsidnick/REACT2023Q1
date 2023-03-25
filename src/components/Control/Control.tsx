6518fbcaca6467515bf74b87d13f677f723d398d
Nick GS <gsidnick@gmail.com>
2023-03-25 09:24:49 +0300

    refactor: implement requests column

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -1,12 +1,26 @@
 import './Control.css';
 import React from 'react';
 import Form from '../Form/Form';
+import { IRequest } from '../../interfaces/IRequest';
+import Request from '../Request/Request';
 
-class Control extends React.Component {
+class Control extends React.Component<object, { requests: IRequest[] }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { requests: [] as IRequest[] };
+    this.setRequests = this.setRequests.bind(this);
+  }
+
+  setRequests(data: IRequest) {
+    const requests = this.state.requests;
+    requests.push(data);
+    this.setState({ requests });
+  }
+
   render() {
     return (
       <div className="control">
-        <div className="control__col">
+        <div className="control__col control__questions">
           <div className="control__info">
             <h2 className="control__title">How About a Free Pizza?</h2>
             <p className="control__text">
@@ -16,10 +30,15 @@ class Control extends React.Component {
               Fill out the questionnaire:
             </p>
           </div>
-          <Form />
+          <Form setRequests={this.setRequests} />
         </div>
-        <div className="control__col">
+        <div className="control__col control__requests">
           <h2 className="control__title">Your Requests</h2>
+          <div className="control__list">
+            {this.state.requests.map((item, index) => (
+              <Request key={index} data={item} />
+            ))}
+          </div>
         </div>
       </div>
     );
