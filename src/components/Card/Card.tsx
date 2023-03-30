004f8cb44a5822e71ae2fce643ab06c7ae4293a7
Nick GS <gsidnick@gmail.com>
2023-03-30 17:06:36 +0300

    refactor: change Card to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,22 +3,17 @@ import React from 'react';
 import Button from '../UI/Button/Button';
 import ICardProps from '../../interfaces/ICardProps';
 
-class Card extends React.Component<ICardProps> {
-  constructor(props: ICardProps) {
-    super(props);
-  }
-  render() {
-    const { image, name, price, description } = this.props.value;
-    return (
-      <div className="card">
-        <img className="card__image" src={`../${image}`} alt={name}></img>
-        <h3 className="card__title">{name}</h3>
-        <span className="card__price">${price}</span>
-        <span className="card__description">{description}</span>
-        <Button className="card__button">Add to cart</Button>
-      </div>
-    );
-  }
+function Card({ ...props }: ICardProps) {
+  const { image, name, price, description } = props.value;
+  return (
+    <div className="card">
+      <img className="card__image" src={`../${image}`} alt={name}></img>
+      <h3 className="card__title">{name}</h3>
+      <span className="card__price">${price}</span>
+      <span className="card__description">{description}</span>
+      <Button className="card__button">Add to cart</Button>
+    </div>
+  );
 }
 
 export default Card;
