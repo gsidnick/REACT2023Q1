c597509c4f74ba705ad2179205c81bb17937637c
Nick GS <gsidnick@gmail.com>
2023-03-30 16:59:36 +0300

    refactor: change Header to functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,24 +3,22 @@ import React from 'react';
 import Heading from '../Heading/Heading';
 import Logo from '../Logo/Logo';
 import Menu from '../Menu/Menu';
-import { IWithRouterProps } from '../../hoc/IWithRouterProps';
-import withRouter from '../../hoc/withRouter';
 import { getPageName } from '../../utils/page';
+import { useLocation } from 'react-router-dom';
 
-class Header extends React.Component<IWithRouterProps> {
-  render() {
-    const pageName = getPageName(this.props.location.pathname);
+function Header() {
+  const location = useLocation();
+  const pageName = getPageName(location.pathname);
 
-    return (
-      <header className="header">
-        <div className="header__container container">
-          <Logo />
-          <Heading title={pageName} />
-          <Menu />
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="header">
+      <div className="header__container container">
+        <Logo />
+        <Heading title={pageName} />
+        <Menu />
+      </div>
+    </header>
+  );
 }
 
-export default withRouter(Header);
+export default Header;
