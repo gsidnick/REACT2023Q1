a3657eb4a54a3d1318d566ea0ccaaf37f8425d62
Nick GS <gsidnick@gmail.com>
2023-03-30 15:24:10 +0300

    refactor: change NotFound to functional component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,27 +1,24 @@
 import './NotFound.css';
 import React from 'react';
 import Button from '../UI/Button/Button';
-import { IWithRouterProps } from '../../hoc/IWithRouterProps';
-import withRouter from '../../hoc/withRouter';
+import { useNavigate } from 'react-router-dom';
 
-class NotFound extends React.Component<IWithRouterProps> {
-  render() {
-    const navigate = this.props.navigate;
+function NotFound() {
+  const navigate = useNavigate();
 
-    return (
-      <div className="not-found">
-        <h1 className="not-found__title">404</h1>
-        <h2 className="not-found__subtitle">Page Not Found</h2>
-        <span className="not-found__message">
-          <p>We didn’t find this page.</p>
-          <p>But we know where to find a lot of delicious things.</p>
-        </span>
-        <Button className="not-found__button" onClick={() => navigate('/')}>
-          Go to home
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <h2 className="not-found__subtitle">Page Not Found</h2>
+      <span className="not-found__message">
+        <p>We didn’t find this page.</p>
+        <p>But we know where to find a lot of delicious things.</p>
+      </span>
+      <Button className="not-found__button" onClick={() => navigate('/')}>
+        Go to home
+      </Button>
+    </div>
+  );
 }
 
-export default withRouter(NotFound);
+export default NotFound;
