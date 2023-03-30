f47ac913acd996f77065c5e7c45de39e54207323
Nick GS <gsidnick@gmail.com>
2023-03-30 15:30:14 +0300

    refactor: change About to functional component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,26 +1,23 @@
 import './About.css';
 import React from 'react';
 
-class About extends React.Component {
-  render() {
-    return (
-      <div className="about">
-        <img className="about__image" src="../pizza.jpg" alt="" />
-        <div className="about__text">
-          <p>
-            Usually people come to Pizza’s Cafe just to eat. Our promoters distribute leaflets about
-            a slice of pizza for twenty rubles or something else profitable. We are doing this as
-            the first step to get to know each other.
-          </p>
-          <p>
-            But for us, Pizza’s Cafe is not just pizza. It’s pizza too, but first of all it’s a big
-            deal that inspires us, makes us wake up every morning and continue working with
-            interest.
-          </p>
-        </div>
+function About() {
+  return (
+    <div className="about">
+      <img className="about__image" src="../pizza.jpg" alt="" />
+      <div className="about__text">
+        <p>
+          Usually people come to Pizza’s Cafe just to eat. Our promoters distribute leaflets about a
+          slice of pizza for twenty rubles or something else profitable. We are doing this as the
+          first step to get to know each other.
+        </p>
+        <p>
+          But for us, Pizza’s Cafe is not just pizza. It’s pizza too, but first of all it’s a big
+          deal that inspires us, makes us wake up every morning and continue working with interest.
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default About;
