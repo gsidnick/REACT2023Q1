9f739e51db8d38bfec5c5b3e6c401104058a3e86
Nick GS <gsidnick@gmail.com>
2023-03-30 17:01:49 +0300

    refactor: change Content to functional component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,17 +2,12 @@ import './Content.css';
 import React from 'react';
 import { IContentProps } from '../../interfaces/IContentProps';
 
-class Content extends React.Component<IContentProps, object> {
-  constructor(props: IContentProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <main className="content">
-        <div className="content__container container">{this.props?.children}</div>
-      </main>
-    );
-  }
+function Content({ ...props }: IContentProps) {
+  return (
+    <main className="content">
+      <div className="content__container container">{props.children}</div>
+    </main>
+  );
 }
 
 export default Content;
