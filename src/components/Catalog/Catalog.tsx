9cfaaa31089ce9b940348b56127c1a0b32ee3d5c
Nick GS <gsidnick@gmail.com>
2023-03-30 15:20:11 +0300

    refactor: change Catalog to functional component

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,67 +1,58 @@
 import './Catalog.css';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Input from '../UI/Input/Input';
 import List from '../List/List';
 import data from '../../../data.json';
 import IProduct from '../../interfaces/IProduct';
-import { getQuery, setQuery } from '../../utils/localStorage';
+import { getLocalQuery, setLocalQuery } from '../../utils/localStorage';
 
-class Catalog extends React.Component<object, { query: string; products: IProduct[] }> {
-  private products: IProduct[];
-  constructor(props: object) {
-    super(props);
-    this.products = data;
-    this.state = { query: '', products: [] as IProduct[] };
-    this.searchChangeHandler = this.searchChangeHandler.bind(this);
-    this.applyFilter = this.applyFilter.bind(this);
-  }
-  componentDidMount() {
-    let query = getQuery();
-    if (query === null) query = '';
-    this.setState({ query });
-    this.applyFilter(query);
+function Catalog() {
+  const [query, setQuery] = useState<string>('');
+  const [products, setProducts] = useState<IProduct[]>([] as IProduct[]);
+
+  function searchChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+    applyFilter(event.target.value);
   }
 
-  componentWillUnmount() {
-    setQuery(this.state.query);
-  }
+  const applyFilter = useCallback(
+    (value: string) => {
+      if (value === '') {
+        setProducts(data);
+        return;
+      }
+      const filteredProducts = data.filter((product) => {
+        const name = product.name.toLowerCase();
+        const description = product.description.toLowerCase();
+        const price = product.price;
+        const q = value.toLowerCase();
+        return name.includes(q) || price.toString().includes(q) || description.includes(q);
+      });
+      setProducts(filteredProducts);
+    },
+    [products]
+  );
 
-  searchChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ query: event.target.value });
-    this.applyFilter(event.target.value);
-  }
+  useEffect(() => {
+    const queryString = getLocalQuery();
+    setQuery(queryString);
+    applyFilter(queryString);
+  }, []);
 
-  applyFilter(query: string) {
-    if (query === '') {
-      this.setState({ products: this.products });
-      return;
-    }
-    const filteredProducts = this.products.filter((product) => {
-      const name = product.name.toLowerCase();
-      const description = product.description.toLowerCase();
-      const price = product.price;
-      const q = query.toLowerCase();
-      return name.includes(q) || price.toString().includes(q) || description.includes(q);
-    });
-    this.setState({ products: filteredProducts });
-  }
-  render() {
-    return (
-      <div className="catalog">
-        <div className="catalog__search">
-          <Input
-            type="text"
-            placeholder="Search"
-            value={this.state.query}
-            onChange={this.searchChangeHandler}
-          />
-        </div>
-        <div className="catalog__products">
-          <List value={this.state.products} />
-        </div>
+  useEffect(() => {
+    setLocalQuery(query);
+  }, [query]);
+
+  return (
+    <div className="catalog">
+      <div className="catalog__search">
+        <Input type="text" placeholder="Search" value={query} onChange={searchChangeHandler} />
       </div>
-    );
-  }
+      <div className="catalog__products">
+        <List value={products} />
+      </div>
+    </div>
+  );
 }
 
 export default Catalog;
