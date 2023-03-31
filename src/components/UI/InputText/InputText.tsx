07b5295c30d8e79158aaacc4e3cd6e000a2ada64
Nick GS <gsidnick@gmail.com>
2023-03-31 12:41:20 +0300

    refactor: change InputText to functional component

--- src/components/UI/InputText/InputText.tsx
+++ src/components/UI/InputText/InputText.tsx
@@ -2,28 +2,23 @@ import './InputText.css';
 import React from 'react';
 import { IInputProps } from '../../../interfaces/IInputProps';
 
-class InputText extends React.Component<Omit<IInputProps, 'type'>> {
-  constructor(props: Omit<IInputProps, 'type'>) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="input">
-        <input
-          className="input__field"
-          type="text"
-          name={this.props.name}
-          ref={this.props.forwardedRef}
-          placeholder={this.props.placeholder}
-          value={this.props.value}
-          onChange={this.props.onChange}
-        />
-      </div>
-    );
-  }
+function InputText(
+  { ...props }: Omit<IInputProps, 'type'>,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <div className="input">
+      <input
+        className="input__field"
+        type="text"
+        name={props.name}
+        ref={ref}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </div>
+  );
 }
 
-export default React.forwardRef<HTMLInputElement, Omit<IInputProps, 'type'>>((props, ref) => (
-  <InputText forwardedRef={ref} {...props} />
-));
+export default React.forwardRef(InputText);
