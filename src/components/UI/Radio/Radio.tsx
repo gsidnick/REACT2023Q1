4ec8d4ff8cbb355d64e29ba1ea5d2e58cdde10c5
Nick GS <gsidnick@gmail.com>
2023-03-31 12:43:13 +0300

    refactor: change Radio to functional component

--- src/components/UI/Radio/Radio.tsx
+++ src/components/UI/Radio/Radio.tsx
@@ -3,31 +3,24 @@ import React from 'react';
 import { IRadioProps } from '../../../interfaces/IRadioProps';
 import IconRadio from '../Icon/IconRadio/IconRadio';
 
-class Radio extends React.Component<IRadioProps> {
-  constructor(props: IRadioProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <label className="radio">
-        <input
-          className="radio__field"
-          type="radio"
-          name={this.props.name}
-          value={this.props.value}
-          ref={this.props.forwardedRef}
-        />
-        <span className="radio__wrapper">
-          <span className="radio__icon">
-            <IconRadio />
-          </span>
-          <span className="radio__label">{this.props?.children}</span>
+function Radio({ ...props }: IRadioProps, ref: React.ForwardedRef<HTMLInputElement>) {
+  return (
+    <label className="radio">
+      <input
+        className="radio__field"
+        type="radio"
+        name={props.name}
+        value={props.value}
+        ref={ref}
+      />
+      <span className="radio__wrapper">
+        <span className="radio__icon">
+          <IconRadio />
         </span>
-      </label>
-    );
-  }
+        <span className="radio__label">{props.children}</span>
+      </span>
+    </label>
+  );
 }
 
-export default React.forwardRef<HTMLInputElement, IRadioProps>((props, ref) => (
-  <Radio forwardedRef={ref} {...props} />
-));
+export default React.forwardRef(Radio);
