0fa592f6fbef9f7c1a872f0bae1f2afca33bd062
Nick GS <gsidnick@gmail.com>
2023-03-31 12:38:45 +0300

    refactor: change InputFile to functional component

--- src/components/UI/InputFile/InputFile.tsx
+++ src/components/UI/InputFile/InputFile.tsx
@@ -1,39 +1,31 @@
 import './InputFile.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { IInputFileProps } from '../../../interfaces/IInputFileProps';
 
-class InputFile extends React.Component<IInputFileProps, { filename: string }> {
-  constructor(props: IInputFileProps) {
-    super(props);
-    this.state = { filename: '' };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-  }
+function InputFile({ ...props }: IInputFileProps, ref: React.ForwardedRef<HTMLInputElement>) {
+  const [filename, setFilename] = useState('');
 
-  onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+  function onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     if (event.target.files !== null) {
-      this.setState({ filename: event.target.files[0].name });
+      setFilename(event.target.files[0].name);
     }
-    if (this.props.onChange != undefined) this.props.onChange(event);
+    if (props.onChange != undefined) props.onChange(event);
   }
 
-  render() {
-    return (
-      <label className="input-file">
-        <input
-          className="input-file__field"
-          type="file"
-          accept="image/png, image/jpeg"
-          name={this.props.name}
-          ref={this.props.forwardedRef}
-          onChange={this.onChangeHandler}
-        />
-        <span className="input-file__name">{this.state.filename}</span>
-        <span className="input-file__button button">{this.props.label}</span>
-      </label>
-    );
-  }
+  return (
+    <label className="input-file">
+      <input
+        className="input-file__field"
+        type="file"
+        accept="image/png, image/jpeg"
+        name={props.name}
+        ref={ref}
+        onChange={onChangeHandler}
+      />
+      <span className="input-file__name">{filename}</span>
+      <span className="input-file__button button">{props.label}</span>
+    </label>
+  );
 }
 
-export default React.forwardRef<HTMLInputElement, IInputFileProps>((props, ref) => (
-  <InputFile forwardedRef={ref} {...props} />
-));
+export default React.forwardRef(InputFile);
