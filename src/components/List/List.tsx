0fe554ed7051234f80d5158e4ca5d805cedf8dd8
Nick GS <gsidnick@gmail.com>
2023-03-31 12:10:31 +0300

    refactor: change List to functional component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,19 +3,14 @@ import React from 'react';
 import Card from '../Card/Card';
 import { IListProps } from '../../interfaces/IListProps';
 
-class List extends React.Component<IListProps> {
-  constructor(props: IListProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="list">
-        {this.props.value.map((product) => (
-          <Card key={product.id} value={product} />
-        ))}
-      </div>
-    );
-  }
+function List({ ...props }: IListProps) {
+  return (
+    <div className="list">
+      {props.value.map((product) => (
+        <Card key={product.id} value={product} />
+      ))}
+    </div>
+  );
 }
 
 export default List;
