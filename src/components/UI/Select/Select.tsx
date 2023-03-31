2ac12feaa965df815e748d5dd828a973c62c569b
Nick GS <gsidnick@gmail.com>
2023-03-31 12:47:09 +0300

    refactor: change Select to functional component

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -2,27 +2,19 @@ import './Select.css';
 import React from 'react';
 import { ISelectProps } from '../../../interfaces/ISelectProps';
 
-class Select extends React.Component<ISelectProps> {
-  private options: string[];
-  constructor(props: ISelectProps) {
-    super(props);
-    this.options = ['Rome', 'Milan', 'Turin', 'Palermo', 'Florence'];
-  }
+function Select({ ...props }: ISelectProps, ref: React.ForwardedRef<HTMLSelectElement>) {
+  const options = ['Rome', 'Milan', 'Turin', 'Palermo', 'Florence'];
 
-  render() {
-    return (
-      <select className="select" ref={this.props.forwardedRef} name={this.props.name}>
-        <option value="">Choose city</option>
-        {this.options.map((item, index) => (
-          <option key={index} value={item}>
-            {item}
-          </option>
-        ))}
-      </select>
-    );
-  }
+  return (
+    <select className="select" ref={ref} name={props.name}>
+      <option value="">Choose city</option>
+      {options.map((item, index) => (
+        <option key={index} value={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  );
 }
 
-export default React.forwardRef<HTMLSelectElement, ISelectProps>((props, ref) => (
-  <Select forwardedRef={ref} {...props} />
-));
+export default React.forwardRef(Select);
