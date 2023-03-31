e0205485d58a92208bac0038503eb146535a8ff7
Nick GS <gsidnick@gmail.com>
2023-03-31 12:59:58 +0300

    refactor: change Control to functional component

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -1,48 +1,42 @@
 import './Control.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../Form/Form';
 import { IRequest } from '../../interfaces/IRequest';
 import Request from '../Request/Request';
 
-class Control extends React.Component<object, { requests: IRequest[] }> {
-  constructor(props: object) {
-    super(props);
-    this.state = { requests: [] as IRequest[] };
-    this.setRequests = this.setRequests.bind(this);
+function Control() {
+  const [requests, setRequests] = useState<IRequest[]>([]);
+
+  function setRequestsHandler(data: IRequest) {
+    const tempRequest = [...requests];
+    tempRequest.push(data);
+    setRequests(tempRequest);
   }
 
-  setRequests(data: IRequest) {
-    const requests = this.state.requests;
-    requests.push(data);
-    this.setState({ requests });
-  }
-
-  render() {
-    return (
-      <div className="control">
-        <div className="control__col control__questions">
-          <div className="control__info">
-            <h2 className="control__title">How About a Free Pizza?</h2>
-            <p className="control__text">
-              Mystery shoppers and inspectors are our agency network that helps to find violations
-              in Pizza’s Cafe pizzerias.
-              <br />
-              Fill out the questionnaire:
-            </p>
-          </div>
-          <Form setRequests={this.setRequests} />
+  return (
+    <div className="control">
+      <div className="control__col control__questions">
+        <div className="control__info">
+          <h2 className="control__title">How About a Free Pizza?</h2>
+          <p className="control__text">
+            Mystery shoppers and inspectors are our agency network that helps to find violations in
+            Pizza’s Cafe pizzerias.
+            <br />
+            Fill out the questionnaire:
+          </p>
         </div>
-        <div className="control__col control__requests">
-          <h2 className="control__title">Your Requests</h2>
-          <div className="control__list">
-            {this.state.requests.map((item, index) => (
-              <Request key={index} data={item} />
-            ))}
-          </div>
+        <Form setRequests={setRequestsHandler} />
+      </div>
+      <div className="control__col control__requests">
+        <h2 className="control__title">Your Requests</h2>
+        <div className="control__list">
+          {requests.map((item, index) => (
+            <Request key={index} data={item} />
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Control;
