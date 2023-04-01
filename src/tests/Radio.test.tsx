b2169fc20f01b2a9225869f229158be5739fd5ef
Nick GS <gsidnick@gmail.com>
2023-04-01 18:11:18 +0300

    refactor: rename Radio component test

--- src/tests/Radio.test.tsx
+++ src/tests/Radio.test.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { describe, test, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Radio from '../components/UI/Radio/Radio';
 
 describe('Testing <Radio /> component', () => {
-  test('should show Radio component', () => {
-    const component = render(<Radio name="radioButton" value="radioValue" />);
-    expect(component).toBeTruthy();
+  test('should show Radio component with input text', () => {
+    render(
+      <Radio name="gender" value="Male">
+        Male
+      </Radio>
+    );
+    expect(screen.getByText(/.*/i, { selector: 'input[name="gender"]' })).toBeTruthy();
+  });
+  test('should show Radio component with input text', () => {
+    render(
+      <Radio name="gender" value="Male">
+        Male
+      </Radio>
+    );
+    expect(screen.getByText(/Male/i)).toBeDefined();
   });
 });
