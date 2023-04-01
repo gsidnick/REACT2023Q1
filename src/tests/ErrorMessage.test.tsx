7258d1499ab75ec3c20a5beeb9cc6c35f00d0b0c
Nick GS <gsidnick@gmail.com>
2023-04-01 20:49:27 +0300

    refactor: change ErrorMessage component test

--- src/tests/ErrorMessage.test.tsx
+++ src/tests/ErrorMessage.test.tsx
@@ -4,19 +4,11 @@ import { render, screen } from '@testing-library/react';
 import Request from '../components/Request/Request';
 import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 
-const mockData = [
-  'Birthday is required',
-  'Age should be over 18 years old',
-  'Age should be less 100 years old',
-  'Date should not be in the future',
-];
+const mockData = 'Birthday is required';
 
 describe('Testing <ErrorMessage /> component', () => {
   test('should show Radio component with input text', () => {
-    render(<ErrorMessage errorMessages={mockData} />);
+    render(<ErrorMessage errorMessage={mockData} />);
     expect(screen.getByText(/Birthday is required/i)).toBeInTheDocument();
-    expect(screen.getByText(/Age should be over 18 years old/i)).toBeInTheDocument();
-    expect(screen.getByText(/Age should be less 100 years old/i)).toBeInTheDocument();
-    expect(screen.getByText(/Date should not be in the future/i)).toBeInTheDocument();
   });
 });
