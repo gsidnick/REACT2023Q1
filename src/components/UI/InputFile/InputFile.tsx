0c2fcd377954e3a11322a911e2dbfb61d2b91c8e
Nick GS <gsidnick@gmail.com>
2023-04-01 18:11:59 +0300

    refactor: change InputFile component

--- src/components/UI/InputFile/InputFile.tsx
+++ src/components/UI/InputFile/InputFile.tsx
@@ -1,30 +1,21 @@
 import './InputFile.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { IInputFileProps } from '../../../interfaces/IInputFileProps';
 
 function InputFile({ ...props }: IInputFileProps, ref: React.ForwardedRef<HTMLInputElement>) {
-  const [filename, setFilename] = useState('');
-
-  function onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.files !== null) {
-      setFilename(event.target.files[0].name);
-    }
-    if (props.onChange != undefined) props.onChange(event);
-  }
-
   return (
-    <label className="input-file">
+    <>
+      <label className="input-file">
+        <span className="form__question-label">{props.label}</span>
+      </label>
       <input
         className="input-file__field"
         type="file"
         accept="image/png, image/jpeg"
-        name={props.name}
+        name="photo"
         ref={ref}
-        onChange={onChangeHandler}
       />
-      <span className="input-file__name">{filename}</span>
-      <span className="input-file__button button">{props.label}</span>
-    </label>
+    </>
   );
 }
 
